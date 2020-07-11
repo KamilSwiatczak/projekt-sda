dedbc2afe3bf2fa0af93303826419a58e9948b24
KamilSwiatczak <57064966+KamilSwiatczak@users.noreply.github.com>
2020-07-11 14:07:51 +0200

    zaczynamy już

---
+++ src/main/java/sda/projektksiegarnia/ProjektKsiegarniaApplication.java
@@ -0,0 +1,13 @@
+package sda.projektksiegarnia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektKsiegarniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektKsiegarniaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sda/projektksiegarnia/ProjektKsiegarniaApplicationTests.java
@@ -0,0 +1,13 @@
+package sda.projektksiegarnia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektKsiegarniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
