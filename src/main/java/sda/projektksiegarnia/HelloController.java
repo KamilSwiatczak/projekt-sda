b11c23026a02ed6795592bd25a51669514008a4b
KamilSwiatczak <57064966+KamilSwiatczak@users.noreply.github.com>
2020-07-18 15:04:27 +0200

    Update HelloController.java

--- src/main/java/sda/projektksiegarnia/HelloController.java
+++ src/main/java/sda/projektksiegarnia/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
     public class HelloController {
         @GetMapping("/")
         String hello() {
-            return "WITAMY W NAJLEPSZEJ KSIĘGARNI NA ŚWIECIE!";
+            return "WITAMY W NAJLEPSZEJ KSIĘGARNI NA ŚWIECIE!!!";
         }
     }
