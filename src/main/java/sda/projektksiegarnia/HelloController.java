8154cc46b9c0c17849e2eb572ce018bd6f4c132d
kielichowska <a.kielichowska@gmail.com>
2020-07-18 12:00:38 +0200

    Update HelloController.java

--- src/main/java/sda/projektksiegarnia/HelloController.java
+++ src/main/java/sda/projektksiegarnia/HelloController.java
@@ -3,18 +3,9 @@ package sda.projektksiegarnia;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class PageController {
-    @GetMapping ("/")
-    String Page(){
-        return "Bookstore page - coming soon.";
-    }
-}
-
-
     @RestController
     public class HelloController {
-        @GetMapping("/books")
+        @GetMapping("/book")
         String hello() {
             return "Tu powinny pojawić się książki. Kiedyś uda nam się je dodać ;) ";
         }
