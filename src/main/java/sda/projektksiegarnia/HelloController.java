7e7a9390e0f1de1aa1b70723c9908888b0b21fc5
KamilSwiatczak <57064966+KamilSwiatczak@users.noreply.github.com>
2020-07-19 11:21:50 +0200

    Update HelloController.java

--- src/main/java/sda/projektksiegarnia/HelloController.java
+++ src/main/java/sda/projektksiegarnia/HelloController.java
@@ -3,10 +3,20 @@ package sda.projektksiegarnia;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-    @RestController
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+@RestController
     public class HelloController {
-        @GetMapping("/")
+        URL linkBooks = new URL("https://projekt-koncowy-bookstore.herokuapp.com/books");
+
+    public HelloController() throws MalformedURLException {
+    }
+
+    @GetMapping("/")
         String hello() {
-            return "WITAMY W NAJLEPSZEJ KSIĘGARNI NA ŚWIECIE!!!" + "\n https://projekt-koncowy-bookstore.herokuapp.com/books";
+            return "WITAMY W NAJLEPSZEJ KSIĘGARNI NA ŚWIECIE!!!\n" + linkBooks;
         }
+
     }
